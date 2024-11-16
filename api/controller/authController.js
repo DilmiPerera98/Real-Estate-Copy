0833a1f707314b4f343d7fac972bbce23e864fe8
Dilmi <dkaushani98@gmail.com>
2024-11-16 22:59:46 +0530

    crete error middleware, errorhandler amd signUp UI

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,7 +1,8 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
   const { userName, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ userName, email, password: hashPassword });
@@ -9,6 +10,9 @@ export const signUp = async (req, res) => {
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
-    res.status(500).json(error.message);
+    //res.status(500).json(error.message);
+    next(error);
+    //if the above error is not defind then we can create a function
+    //next(errorHandler(550, "error form function"));
   }
 };
--- api/index.js
+++ api/index.js
@@ -24,3 +24,11 @@ app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+//create the middleware to handle the errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
